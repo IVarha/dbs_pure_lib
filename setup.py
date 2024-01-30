51fcc63845c91f538f0c7f0cd9fd72aa4e1030ef
IVarha <IVarha@users.noreply.github.com>
2024-01-30 16:09:41 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+
+
+setup(
+    name='dbs_pure_lib',
+    version='',
+    packages=['dbs_image_utils'],
+    url='',
+    license='',
+    author='igor_varha',
+    author_email='ivarhauzh@gmail.com',
+    description=''
+)
