585ea9a80c1b5b75c09b71acf9830cfd303ad46d
Igor Varga <ivarhauzh@gmail.com>
2025-04-29 14:51:50 +0200

    Add setup for a library

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ from setuptools import setup
 
 setup(
     name='dbs_pure_lib',
-    version='',
+    version='0.0.1',
     packages=['dbs_image_utils'],
+    description='DBS Image Utilities',
+    long_description=open("README.md").read(),
+    install_requires=['torch'],
     url='',
     license='',
     author='igor_varha',
     author_email='ivarhauzh@gmail.com',
-    description=''
 )
